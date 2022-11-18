72d2984fd0cd8f438a97e6dce77adfd829509325
Manwinder Singh <200491908@student.georgianc.on.ca>
2022-11-18 11:31:02 -0600

    Fetch movies from api using JavaScript

---
+++ index.js
@@ -0,0 +1,65 @@
+//5e6f1e7705030a57d1cf8a0624b0f539
+
+let movies;
+const search = document.querySelector('.search__input');
+const movieList = document.querySelector('.movies');
+
+async function getMovies(search) {
+    const fetchMovies = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=5e6f1e7705030a57d1cf8a0624b0f539&&query=${search}`);
+    const moviesData = await fetchMovies.json();
+    return moviesData.results;
+}
+
+async function renderMovies (filter) {
+    const data = await getMovies(search.value);
+    
+    movieList.classList += ' movies__loading';
+
+    if (!movies) {
+        movies = getMovies();
+    }
+
+    movieList.classList.remove('movies__loading');
+
+    if (filter === 'SORT_BY_NAME') {
+            const filteredMovies = movies.sort((a, b) => a.title.localeCompare(b.title))
+         }
+         else if (filter === 'SORT_BY_RATING') {
+             const filteredMovies = movies.sort((a, b) => b.vote_average - a.vote_average)
+         }
+         else if (filter === 'SORT_BY_DATE') {
+             const filteredMovies = movies.sort((a, b) => b.release_date - a.release_date)
+         }
+
+    movieList.innerHTML = data.map(movie => {
+        const imageUrl = `https://image.tmdb.org/t/p/w1280` + movie.poster_path;
+            return ` 
+                     <div class="movie">
+                     <div class="movie__header">
+                            <figure class="movie__poster--wrapper">
+                                 <img class="movie__poster" src="${imageUrl}" alt="">
+                            </figure>
+                     </div>
+                     <div class="movie__body">
+                             <span class="genre__tag">${movie.release_date}</span>
+                             <div class="movie__title">${movie.title}</div>
+                             <div class="movie__rating">Vote Average: ${movie.vote_average}</div>
+                             <div class="release__year">Vote Count: ${movie.vote_count}</div>
+                             <div class="release__year">Language: ${movie.original_language}</div>
+                     </div>
+                     </div>`
+        }
+    ).join("")
+}
+
+function filterMovies(event) {
+    renderMovies(event.target.value)
+}
+
+getMovies();
+
+
+
+
+
+
